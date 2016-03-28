f7089e9a27e967fd3e0814c77ed951db575e78cc
Vladimir <v-for-vendeta@hotmail.rs>
2016-03-28 20:55:19 +0200

    Add update item and query params for status

--- server.js
+++ server.js
@@ -16,8 +16,18 @@ app.get('/', function (req, res) {
     res.send("Hello Wrold");
 });
 
+//GET /todos?status=true
 app.get('/todos', function (req, res) {
-    res.json(todos);
+    var queryParams = req.query;
+    var filtredTodos = todos;
+    
+    if (queryParams.hasOwnProperty('status') && queryParams.status === 'true') {
+        filtredTodos = _.where(filtredTodos, {status: true});
+    }else if (queryParams.hasOwnProperty('status') && queryParams.status === 'false'){
+        filtredTodos = _.where(filtredTodos, {status: false});
+    }
+    
+    res.json(filtredTodos);
 });
 
 //GET /todos/:id
@@ -46,17 +56,47 @@ app.post('/todos', function (req, res) {
 });
 
 // DELETE /todos
-app.delete('/todos/:id', function(req, res) {
+app.delete('/todos/:id', function (req, res) {
     var matchItem = _.findWhere(todos, {id: parseInt(req.params.id)});
-    
-    
-    if(!matchItem) {
-        res.status(404).json({"error":"No items"});
+
+
+    if (!matchItem) {
+        res.status(404).json({"error": "No items"});
     } else {
-           todos = _.without(todos, matchItem);
-           res.json(matchItem);
+        todos = _.without(todos, matchItem);
+        res.json(matchItem);
     }
+
+
+
 });
+
+//PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+    var matchItem = _.findWhere(todos, {id: parseInt(req.params.id)});
+    var body = _.pick(req.body, 'description', 'status');
+    var validAttributes = {};
+
+    if (!matchItem) {
+        return res.status(404).send();
+    }
+    if (body.hasOwnProperty('status')) {
+        validAttributes.status = body.status;
+    } else if (body.hasOwnProperty('status')) {
+        return res.status(400).send();
+    }
+
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.descrition = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(matchItem, validAttributes);
+    res.json(matchItem);
+});
+
 
 app.listen(PORT, function () {
     console.log("Server Sterted!");

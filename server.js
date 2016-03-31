e64991d397a4570085d8607c3a4d96a934f1575f
Vladimir <v-for-vendeta@hotmail.rs>
2016-03-31 13:34:26 +0200

    Added CORS

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ var bcrypt = require('bcrypt');
 
 app.use(bodyParser.json());
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/', function (req, res) {
     res.send("Hello Wrold");
 });
